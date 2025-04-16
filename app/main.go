d5bf89c3789f9dc605782eac15de6b49fafffb13
zjpiazza <zjpiazza@gmail.com>
2025-04-15 21:40:44 -0500

    Implement POST /files{str}

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"regexp"
@@ -12,19 +13,12 @@ import (
 	"syscall"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	var directory string
 	flag.StringVar(&directory, "directory", "/tmp", "Directory for files")
 	flag.Parse()
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -34,117 +28,156 @@ func main() {
 
 	for {
 		conn, err := l.Accept()
-
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
-		go func(c net.Conn) {
-			reader := bufio.NewReader(conn)
-			data, err := reader.ReadString('\n')
+		go handleConnection(conn, directory)
+	}
+}
 
-			headers := make(map[string]string)
-			for {
-				line, _ := reader.ReadString('\n')
-				line = strings.TrimSpace(line)
-				if line == "" {
-					break // End of headers
-				}
-				parts := strings.SplitN(line, ":", 2)
-				if len(parts) == 2 {
-					key := strings.TrimSpace(parts[0])
-					value := strings.TrimSpace(parts[1])
-					headers[key] = value
-				}
+func handleConnection(conn net.Conn, directory string) {
+	defer conn.Close() // Ensure connection is always closed when function returns
+
+	reader := bufio.NewReader(conn)
+
+	// Read the request line
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Failed to read request:", err)
+		conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+		return
+	}
+
+	parts := strings.Split(strings.TrimSpace(requestLine), " ")
+	if len(parts) < 3 {
+		fmt.Println("Invalid request format")
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
+	}
+
+	method := parts[0]
+	url := parts[1]
+
+	// Parse headers
+	headers := make(map[string]string)
+	contentLength := 0
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading headers:", err)
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
+
+		line = strings.TrimSpace(line)
+		if line == "" {
+			break // End of headers
+		}
+
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			headers[key] = value
+
+			if strings.ToLower(key) == "content-length" {
+				fmt.Sscanf(value, "%d", &contentLength)
 			}
+		}
+	}
 
-			if err != nil {
-				fmt.Println("Failed to read request")
-			}
+	// Read body if needed (for POST requests)
+	var body []byte
+	if method == "POST" && contentLength > 0 {
+		body = make([]byte, contentLength)
+		_, err := io.ReadFull(reader, body)
+		if err != nil {
+			fmt.Println("Error reading request body:", err)
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
+	}
 
-			parts := strings.Split(data, " ")
+	echoPath := regexp.MustCompile(`^/echo/[^/]+$`)
+	filesPath := regexp.MustCompile(`^/files/[^/]+$`)
 
-			url := parts[1]
+	if url == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if echoPath.MatchString(url) {
+		responseBody := strings.TrimSpace(strings.TrimPrefix(url, "/echo/"))
+		response := fmt.Sprintf(
+			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+			len(responseBody),
+			responseBody,
+		)
+		conn.Write([]byte(response))
+	} else if url == "/user-agent" {
+		userAgent := headers["User-Agent"]
+		response := fmt.Sprintf(
+			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+			len(userAgent),
+			userAgent,
+		)
+		conn.Write([]byte(response))
+	} else if filesPath.MatchString(url) && method == "GET" {
+		fileName := strings.TrimSpace(strings.TrimPrefix(url, "/files/"))
+		filePath := fmt.Sprintf("%s/%s", directory, fileName)
 
-			echoPath := regexp.MustCompile(`^/echo/[^/]+$`)
-			filesPath := regexp.MustCompile(`^/files/[^/]+$`)
+		// Try to open the file
+		f, err := os.Open(filePath)
 
-			if url == "/" {
-				conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-			} else if echoPath.MatchString(url) {
-				responseBody := strings.TrimSpace(strings.TrimPrefix(url, "/echo/"))
-				response := fmt.Sprintf(
-					"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-					len(responseBody),
-					responseBody,
-				)
+		// If file doesn't exist, return 404
+		if errors.Is(err, syscall.ENOENT) {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\nContent-Type: application/octet-stream\r\nContent-Length: 0\r\n\r\n"))
+			return
+		}
 
-				conn.Write([]byte(response))
-			} else if url == "/user-agent" {
-				userAgent := headers["User-Agent"]
-				response := fmt.Sprintf(
-					"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-					len(userAgent),
-					userAgent,
-				)
+		// If there was some other error opening the file
+		if err != nil {
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
 
-				conn.Write([]byte(response))
-			} else if filesPath.MatchString(url) {
-				fileName := strings.TrimSpace(strings.TrimPrefix(url, "/files/"))
-				filePath := fmt.Sprintf("%s/%s", directory, fileName)
-				fmt.Println(filePath)
+		// Make sure we close the file when done
+		defer f.Close()
 
-				// Try to open the file
-				f, err := os.Open(filePath)
+		// Read file contents
+		b, err := os.ReadFile(filePath)
+		if err != nil {
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
 
-				// If file doesn't exist, return 404 immediately
-				if errors.Is(err, syscall.ENOENT) {
-					conn.Write([]byte("HTTP/1.1 404 Not Found\r\nContent-Type: application/octet-stream\r\nContent-Length: 0\r\n\r\n"))
-					c.Close()
-					return
-				}
+		// Get file size
+		fi, err := f.Stat()
+		if err != nil {
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
 
-				// If there was some other error opening the file
-				if err != nil {
-					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-					c.Close()
-					return
-				}
+		size := fi.Size()
+		response := fmt.Sprintf(
+			"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n",
+			size,
+		)
+		conn.Write([]byte(response))
+		conn.Write(b)
+	} else if filesPath.MatchString(url) && method == "POST" {
+		fileName := strings.TrimSpace(strings.TrimPrefix(url, "/files/"))
+		filePath := fmt.Sprintf("%s/%s", directory, fileName)
 
-				// Make sure we close the file when done
-				defer f.Close()
+		// Write the file to the directory
+		err := os.WriteFile(filePath, body, 0644)
+		if err != nil {
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
 
-				// Read file contents
-				b, err := os.ReadFile(filePath)
-				if err != nil {
-					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-					c.Close()
-					return
-				}
-
-				// Get file size
-				fi, err := f.Stat()
-				if err != nil {
-					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-					c.Close()
-					return
-				}
-
-				size := fi.Size()
-				response := fmt.Sprintf(
-					"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s",
-					size,
-					string(b),
-				)
-				conn.Write([]byte(response))
-				c.Close()
-				return
-			} else {
-				conn.Write([]byte("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n"))
-			}
-
-			c.Close()
-		}(conn)
+		conn.Write([]byte("HTTP/1.1 201 Created\r\nContent-Length: 0\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n"))
 	}
 }
